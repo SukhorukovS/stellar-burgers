56c41a50e90a64196f52c32d0acb24b59084b0d5
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-07 08:26:01 +0400

    Close public routes

--- src/components/routes/public-route.tsx
+++ src/components/routes/public-route.tsx
@@ -1,8 +1,16 @@
+import { useSelector } from '@services/store';
+import { getIsInitialState, getIsLoading, getUser } from '@slices/user';
+import { Preloader } from '@ui';
 import { Navigate } from 'react-router-dom';
 
 export const PublicRoute = ({ children }: { children: JSX.Element }) => {
-  // TODO: проверка авторизации из стора
-  const isAuthenticated = false;
+  const isLoading = useSelector(getIsLoading);
+  const isInitialState = useSelector(getIsInitialState);
+  const isAuthenticated = Boolean(useSelector(getUser));
+
+  if (isLoading || isInitialState) {
+    return <Preloader />;
+  }
 
   if (isAuthenticated) {
     return <Navigate to='/' replace />;
