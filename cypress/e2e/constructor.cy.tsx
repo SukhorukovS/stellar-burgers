eb6009048a66a4094ddade02d747895f389f3844
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-28 09:35:47 +0400

    Add check for adding ingredients to constructor

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,4 +1,8 @@
-describe('The Home Page', () => {
+import ingredidents from '../fixtures/ingredients.json';
+
+const ingredidentsData = ingredidents.data;
+
+describe('Constructor Page', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/api/ingredients', {
       fixture: 'ingredients.json'
@@ -16,5 +20,31 @@ describe('The Home Page', () => {
     cy.contains('Краторная булка N-200i').should('exist');
     cy.contains('Биокотлета из марсианской Магнолии').should('exist');
     cy.contains('Соус Spicy-X').should('exist');
+  });
+
+  it('could add ingredient to constructor', () => {
+    const bun = ingredidentsData.find(
+      (ingredient) => ingredient.type === 'bun'
+    )!;
+    const bunElement = cy.get(`[data-cy=ingredient-${bun._id}]`);
+    const addButtonBunElement = bunElement.find('button');
+
+    const mainIngredient = ingredidentsData.find(
+      (ingredient) => ingredient.type === 'main'
+    )!;
+    const mainElement = cy.get(`[data-cy=ingredient-${mainIngredient._id}]`);
+    const addButtonMainElement = mainElement.find('button');
+
+    addButtonBunElement.click();
+
+    cy.contains('Выберите булки').should('not.exist');
+
+    addButtonMainElement.click();
+
+    cy.contains('Выберите начинку').should('not.exist');
+
+    const burgerConstructorElement = cy.get('[data-cy=burger-constructor]');
+    burgerConstructorElement.should('contain', bun.name);
+    burgerConstructorElement.should('contain', mainIngredient.name);
   });
 });
