4845572f2470fc3b4c41a28769abd65d8f443f3a
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-28 09:41:26 +0400

    Add ,odal testing

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -48,3 +48,53 @@ describe('Constructor Page', () => {
     burgerConstructorElement.should('contain', mainIngredient.name);
   });
 });
+
+describe('Ingredient Modal', () => {
+  const ingredient = ingredidentsData[0];
+
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit('/');
+    cy.wait('@getIngredients');
+  });
+
+  it('should open ingredient modal when clicking on ingredient', () => {
+    cy.get(`[data-cy=ingredient-${ingredient._id}]`).find('a').click();
+
+    cy.contains('Детали ингредиента').should('exist');
+    cy.contains(ingredient.name).should('exist');
+    cy.contains(String(ingredient.calories)).should('exist');
+    cy.contains(String(ingredient.proteins)).should('exist');
+    cy.contains(String(ingredient.fat)).should('exist');
+    cy.contains(String(ingredient.carbohydrates)).should('exist');
+    cy.url().should(
+      'eq',
+      `${Cypress.config('baseUrl')}/ingredients/${ingredient._id}`
+    );
+  });
+
+  it('should close modal when clicking the close button', () => {
+    cy.get(`[data-cy=ingredient-${ingredient._id}]`).find('a').click();
+
+    cy.contains('Детали ингредиента').should('exist');
+
+    cy.get('[data-cy=modal-close]').click();
+
+    cy.contains('Детали ингредиента').should('not.exist');
+    cy.url().should('eq', `${Cypress.config('baseUrl')}/`);
+  });
+
+  it('should close modal when clicking the overlay', () => {
+    cy.get(`[data-cy=ingredient-${ingredient._id}]`).find('a').click();
+
+    cy.contains('Детали ингредиента').should('exist');
+
+    cy.get('[data-cy=modal-overlay]').click({ force: true });
+
+    cy.contains('Детали ингредиента').should('not.exist');
+    cy.url().should('eq', `${Cypress.config('baseUrl')}/`);
+  });
+});
