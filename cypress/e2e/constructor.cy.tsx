28dcac60a61f568cc65983f895831332098b225b
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-28 09:01:06 +0400

    Fix type errors and add request interceptor

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,20 @@
+describe('The Home Page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
+
+    cy.visit('/');
+    cy.wait('@getIngredients');
+  });
+
+  it('successfully loads', () => {
+    cy.get('@getIngredients').its('response.statusCode').should('eq', 200);
+  });
+
+  it('displays ingredients from mocked data', () => {
+    cy.contains('Краторная булка N-200i').should('exist');
+    cy.contains('Биокотлета из марсианской Магнолии').should('exist');
+    cy.contains('Соус Spicy-X').should('exist');
+  });
+});
