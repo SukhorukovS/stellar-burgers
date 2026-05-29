6491396c25ad8cba912afbed6dc130b68710a6d3
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-29 09:40:28 +0400

    Add unknown action test

--- src/services/root-reducer.test.ts
+++ src/services/root-reducer.test.ts
@@ -44,4 +44,48 @@ describe('rootReducer initialization', () => {
       ordersError: null
     });
   });
+
+  it('should return the initial state when called with undefined state and unknown action', () => {
+    const state = rootReducer(undefined, { type: 'UNKNOWN_ACTION' });
+
+    expect(state).toHaveProperty('ingredients');
+    expect(state).toHaveProperty('burgerConstructor');
+    expect(state).toHaveProperty('feed');
+    expect(state).toHaveProperty('user');
+
+    expect(state.ingredients).toEqual({
+      ingredients: [],
+      isLoading: false,
+      error: null
+    });
+
+    expect(state.burgerConstructor).toEqual({
+      orderRequest: false,
+      error: null,
+      orderData: null,
+      constructorItems: {
+        bun: null,
+        ingredients: []
+      }
+    });
+
+    expect(state.feed).toEqual({
+      data: null,
+      isLoading: false,
+      error: null,
+      currentOrder: null,
+      isLoadingCurrentOrder: false,
+      currentOrderError: null
+    });
+
+    expect(state.user).toEqual({
+      user: null,
+      isInitialState: true,
+      isLoading: false,
+      error: null,
+      orders: null,
+      isOrdersLoading: false,
+      ordersError: null
+    });
+  });
 });
