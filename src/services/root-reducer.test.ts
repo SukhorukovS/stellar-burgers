69149a9bec3a704e6958719b1bac2f68eb06b1db
Sergey Sukhorukov <Se.Sukhorukov@x5.ru>
2026-05-30 12:53:26 +0400

    Fix root reducer test

--- src/services/root-reducer.test.ts
+++ src/services/root-reducer.test.ts
@@ -1,91 +1,29 @@
 import { rootReducer } from './root-reducer';
+import { ingredientsReducer } from './slices/ingredients';
+import { burgerConstructorReducer } from './slices/burger-constructor';
+import { feedReducer } from './slices/feeds';
+import { userReducer } from './slices/user';
 
 describe('rootReducer initialization', () => {
-  it('should return the initial state with all slices', () => {
-    const initialState = rootReducer(undefined, { type: '@@INIT' });
-
-    expect(initialState).toHaveProperty('ingredients');
-    expect(initialState).toHaveProperty('burgerConstructor');
-    expect(initialState).toHaveProperty('feed');
-    expect(initialState).toHaveProperty('user');
-
-    expect(initialState.ingredients).toEqual({
-      ingredients: [],
-      isLoading: false,
-      error: null
-    });
-
-    expect(initialState.burgerConstructor).toEqual({
-      orderRequest: false,
-      error: null,
-      orderData: null,
-      constructorItems: {
-        bun: null,
-        ingredients: []
-      }
-    });
-
-    expect(initialState.feed).toEqual({
-      data: null,
-      isLoading: false,
-      error: null,
-      currentOrder: null,
-      isLoadingCurrentOrder: false,
-      currentOrderError: null
-    });
-
-    expect(initialState.user).toEqual({
-      user: null,
-      isInitialState: true,
-      isLoading: false,
-      error: null,
-      orders: null,
-      isOrdersLoading: false,
-      ordersError: null
+  it('initializes the state correctly', () => {
+    const initAction = { type: '@@INIT' };
+    const state = rootReducer(undefined, initAction);
+    expect(state).toEqual({
+      burgerConstructor: burgerConstructorReducer(undefined, initAction),
+      feed: feedReducer(undefined, initAction),
+      ingredients: ingredientsReducer(undefined, initAction),
+      user: userReducer(undefined, initAction)
     });
   });
 
-  it('should return the initial state when called with undefined state and unknown action', () => {
-    const state = rootReducer(undefined, { type: 'UNKNOWN_ACTION' });
-
-    expect(state).toHaveProperty('ingredients');
-    expect(state).toHaveProperty('burgerConstructor');
-    expect(state).toHaveProperty('feed');
-    expect(state).toHaveProperty('user');
-
-    expect(state.ingredients).toEqual({
-      ingredients: [],
-      isLoading: false,
-      error: null
-    });
-
-    expect(state.burgerConstructor).toEqual({
-      orderRequest: false,
-      error: null,
-      orderData: null,
-      constructorItems: {
-        bun: null,
-        ingredients: []
-      }
-    });
-
-    expect(state.feed).toEqual({
-      data: null,
-      isLoading: false,
-      error: null,
-      currentOrder: null,
-      isLoadingCurrentOrder: false,
-      currentOrderError: null
-    });
-
-    expect(state.user).toEqual({
-      user: null,
-      isInitialState: true,
-      isLoading: false,
-      error: null,
-      orders: null,
-      isOrdersLoading: false,
-      ordersError: null
+  it('handles unknown action correctly', () => {
+    const fakeAction = { type: 'UNKNOWN_ACTION' };
+    const state = rootReducer(undefined, fakeAction);
+    expect(state).toEqual({
+      burgerConstructor: burgerConstructorReducer(undefined, fakeAction),
+      feed: feedReducer(undefined, fakeAction),
+      ingredients: ingredientsReducer(undefined, fakeAction),
+      user: userReducer(undefined, fakeAction)
     });
   });
 });
